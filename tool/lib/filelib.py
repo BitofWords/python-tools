bac78b211fd9fc4ac71cf4caa74bd5a0e7876874
nkmk <nkmk.on@gmail.com>
2017-07-23 15:23:41 +0900

    add docstring #1

--- tool/lib/filelib.py
+++ tool/lib/filelib.py
@@ -3,6 +3,18 @@ import glob
 
 
 def get_file_list(dir_path, ext="*", is_full_path=False):
+    """
+    Return the list of file paths.
+
+    Args:
+    - dir_path (str): Directory path.
+    - ext (str): Extension. Both include and exclude comma are OK.
+    - is_full_path (bool):Return full pash if True, only filename if False.
+
+    Returns:
+    - (list of str): List of paths.
+    """
+
     files = glob.glob(os.path.join(dir_path, "*.{}".format(ext)))
 
     if is_full_path:
